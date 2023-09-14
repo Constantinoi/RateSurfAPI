0e029e26bc96114ea79496eaf47f74a77898d127
Constantino Barreto <pescaalma@gmail.com>
2023-09-13 21:43:49 -0300

    env: initial config

---
+++ src/main/java/com/ratesurf/RateSurfApiApplication.java
@@ -0,0 +1,13 @@
+package com.ratesurf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateSurfApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RateSurfApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ratesurf/RateSurfApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ratesurf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RateSurfApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
